46e6dea13be73d4fc6063bfcb95c0731c7ec30d5
Leo Song <szzsozhzh@163.com>
2022-03-29 00:01:07 -0400

    Add cnn encoder

--- nn_models/cnn_encoder.py
+++ nn_models/cnn_encoder.py
@@ -2,15 +2,17 @@ from typing import Tuple
 import torch.nn as nn
 
 
-class GRUEncoder(nn.Module):
+class CNNEncoder(nn.Module):
 
-    def __init__(self, input_dim: int, hidden_dims:Tuple[int], outputs:Tuple[int]):
-        super(GRUEncoder, self).__init__()
+    def __init__(self, input_len, input_dim: int, hidden_dims:Tuple[int], outputs:Tuple[int]):
+        super(CNNEncoder, self).__init__()
 
+        self.input_len = input_len
         self.input_dim = input_dim
         self.hidden_dim, self.hidden_dim2 = hidden_dims
         self.output_len, self.output_dim = outputs
 
+        self.cnn = nn.Conv1d(self.input_len, self.output_len, self.input_dim, padding='same')
         self.gru = nn.GRU(self.input_dim, self.hidden_dim, num_layers=2, batch_first=True)
 
         self.linear = nn.Linear(self.hidden_dim, self.hidden_dim2)
@@ -19,8 +21,8 @@ class GRUEncoder(nn.Module):
         
         
     def forward(self, x):
+        x = self.cnn(x)
         x, _ = self.gru(x)
-        x = x[:, -self.output_len:, :]
         x = self.linear(x)
         x = self.relu(x)
         return self.output(x)
